6933d7b6f5e88c29b328701a9681277be21d6cbf
RyanAnderson23 <70349120+RyanAnderson23@users.noreply.github.com>
2020-10-05 16:33:50 -0500

    Initial commit

---
+++ forms/Animals/Animals.js
@@ -0,0 +1,17 @@
+/*Commented out
+
+
+var myAnimals = ["Dog", "Cat", "Horse", "Meerkat"];
+
+let userInput = prompt("Type the name of an animal")
+
+myAnimals.push(userInput);
+
+let fVar = myAnimals.slice(-1).pop();
+var lwrInput = fVar.toLowerCase();
+
+alert('The last animal in the array is a/an ' + fVar);
+
+
+Commented out*/
+
---
+++ forms/Calculate/Calculate.js
@@ -0,0 +1,11 @@
+/*Commented out
+
+var num1 = parseInt(prompt("Enter first value:"));
+var num2 = parseInt(prompt("Enter second value:"));
+var num3 = parseInt(prompt("Enter third value:"));
+
+var numTotal = ((num1 + num2 + num3)/3) * num1 ** 2;
+alert("The answer is: " + numTotal);
+
+
+Commented out*/
